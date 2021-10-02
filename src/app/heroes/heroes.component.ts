95dd4d430733ee3c191040e2824feac33b322655
dtellz <dtellez95@gmail.com>
2021-10-02 21:13:22 +0200

    components creation

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Diego'
+  };
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+  heroes: Hero[] = [];
+
+  // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Jean' },
+    { id: 12, name: 'Veka' },
+    { id: 13, name: 'Paloma' },
+    { id: 14, name: 'Matías' },
+    { id: 15, name: 'Yohe' },
+    { id: 16, name: 'Alex' },
+    { id: 18, name: 'Virginia' },
+    { id: 19, name: 'Jose' },
+    { id: 20, name: 'Marta' },
+    { id: 21, name: 'Jorge' },
+]
