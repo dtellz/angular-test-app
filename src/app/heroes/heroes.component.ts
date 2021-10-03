ae7f832f5663bb51898acc01ed6bbab14a0045ed
dtellz <dtellez95@gmail.com>
2021-10-03 18:38:30 +0200

    router and navigation implemented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-//testing purposes <-ignore
+import { MessageService } from '../message.service'
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,30 +13,22 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Diego'
-  };
   selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 
   heroes: Hero[] = [];
 
   // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes)
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`Component says-> Selected hero is: ${hero.name}`)
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
 }
